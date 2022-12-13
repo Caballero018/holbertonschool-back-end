9856f63d91cbd1976078a3e3e0c5ef0561ea1103
Caballero018 <4617@holbertonstudents.com>
2022-12-13 18:24:36 -0500

    Script to export data in the JSON format.

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -26,10 +26,16 @@ def first_line(id):
             if response[i]['id'] == id:
                 instance["{}".format(id)] = []
                 ins['username'] = response[i]['username']
+
                 for j in range(len(response2)):
                     if response2[j]['userId'] == id:
-                        ins['completed'] = response2[j]['completed']
-                        ins['task'] = response2[j]['title']
+                        if response2[j]['title'] or response2[j]['completed']:
+                            ins = {k: v for k, v in response2[j].items()}
+
+                            if ins['title']:
+                                ins['task'] = ins['title']
+                                del ins['title']
+
                         instance["{}".format(id)].append(ins)
 
         with open("{}.json".format(id), 'w+') as f:
